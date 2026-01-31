904af1f5acb8dd410e6257ae16a61486616a68cf
Cator Vee <hi@catorv.com>
2026-01-31 11:47:27 +0800

    feat: add templates: cmake_exe, cmake_lib

---
+++ templates/cmake_exe/src/main.cpp
@@ -0,0 +1,13 @@
+#include "{{name}}/{{name}}.h"
+#include <vector>
+#include <string>
+
+int main() {
+    {{package_name}}();
+
+    std::vector<std::string> vec;
+    vec.push_back("test_package");
+
+    {{package_name}}_print_vector(vec);
+}
+
